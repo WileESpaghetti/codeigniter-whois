05e41a55276678957cd0915e4de522bfc6f6c267
Lehman Black <dosnlinux@gmail.com>
2015-03-08 21:27:53 -0500

    CodeIgniter library boilerplate from the docs

---
+++ src/Llbci_whois.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Llbci_whois {
+
+    public function some_function()
+    {
+    }
+}
+
+/* End of file Llbci_whois.php */
