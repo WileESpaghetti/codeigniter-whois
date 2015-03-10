3517cdfc14ff18487ec811716db5c31753f1f37d
Lehman Black <dosnlinux@gmail.com>
2015-03-09 19:57:14 -0500

    added skeleton of the library

--- src/Llbci_whois.php
+++ src/Llbci_whois.php
@@ -2,8 +2,53 @@
 
 class Llbci_whois {
 
-    public function some_function()
+    /**
+     * Query a WhoIs server
+     *
+     * @param $domain
+     * @return string
+     */
+    public function query($domain, $fallback = FALSE)
     {
+        // TODO find responsible WhoIs Server
+        // TODO find best way to query server (HTTP or WhoIs Protocol), prefer HTTP
+        // TODO query server
+        return NULL;
+    }
+
+    /**
+     * Make a WhoIs query using HTTP instead of the WhoIs Protocol
+     * @param $domain
+     * @param $server
+     */
+    public function http_query($domain, $server)
+    {
+
+    }
+
+    /**
+     * Make a WhoIs query using the WhoIs protocol
+     *
+     * @param $domain
+     * @param $server
+     *
+     * @see https://tools.ietf.org/html/rfc3912
+     */
+    public function protocol_query($domain, $server)
+    {
+
+    }
+
+    /**
+     * Make a WhoIs query using the whois command
+     *
+     * @param $domain
+     *
+     * @see https://tools.ietf.org/html/rfc3912
+     */
+    public function cli_query($domain)
+    {
+
     }
 }
 
